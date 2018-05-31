cb59d2be730cc7f6d4ba272d1bca83e67d09539a
crosscripter <crosscripter@gmail.com>
2018-05-31 14:38:19 -0500

    Initial commit

---
+++ reverse/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 39);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(522, 115);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.Location = new System.Drawing.Point(12, 160);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.richTextBox2.Size = new System.Drawing.Size(522, 142);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 308);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(96, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Case Sensitive";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(118, 308);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(129, 17);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Puncutation Sensitive";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(441, 14);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(93, 17);
+            this.checkBox3.TabIndex = 4;
+            this.checkBox3.Text = "Hide Scripture";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(423, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 341);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.richTextBox2);
+            this.Name = "Form1";
+            this.Text = "ReVerse";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ reverse/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, string> Scriptures = new Dictionary<string, string>();
+        const string Path = "scriptures.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Read in scriptures.
+            try
+            {
+                string[] lines = File.ReadAllLines(Path);
+
+                foreach (string line in lines)
+                {
+                    string[] parts = line.Split('=');
+                    string verse = parts[0];
+                    string text = parts[1];
+
+                    verse = verse.Trim();
+                    text = text.Trim();
+                    Scriptures.Add(verse, text);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Could not read in scripture list.");
+                Application.Exit();
+            }
+
+            comboBox1.DataSource = Scriptures.Keys.ToList();
+        }
+
+        bool CompareText(bool caseSensative = false, bool punctuationSensative = false)
+        {
+            string a = richTextBox1.Text;
+            string b = richTextBox2.Text;
+
+            if (a.Length == 0 || b.Length == 0) return true;
+
+            int length = (b.Length < a.Length) ? b.Length : a.Length;
+
+            a = a.Substring(0, length);
+
+            if (!caseSensative)
+            {
+                a = a.ToUpper();
+                b = b.ToUpper();
+            }
+
+            if (!punctuationSensative)
+            {
+                a = Regex.Replace(a, @"\W+", string.Empty);
+                b = Regex.Replace(b, @"\W+", string.Empty);
+            }
+
+            a = a.Trim();
+            b = b.Trim();
+
+            if (a == b && richTextBox1.TextLength == richTextBox2.TextLength
+                && caseSensative == true && punctuationSensative == true)
+            {
+                MessageBox.Show("That is correct!");
+            }
+
+            return (a == b);
+        }
+
+        private void ChangeTextColor()
+        {
+            bool caseValue = checkBox1.Checked;
+            bool puncValue = checkBox2.Checked;
+
+            richTextBox2.ForeColor = CompareText(caseValue, puncValue) ? Color.Green : Color.Red;
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+            ChangeTextColor();
+        }
+
+
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            ChangeTextColor();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            ChangeTextColor();
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Visible = !checkBox3.Checked;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == -1) return;
+            string verse = comboBox1.Text;
+
+            try
+            {
+                richTextBox1.Text = Scriptures[verse];
+            }
+            catch
+            {
+                richTextBox1.Text = string.Format("Could not load {0}", verse);
+            }
+        }
+    }
+}
